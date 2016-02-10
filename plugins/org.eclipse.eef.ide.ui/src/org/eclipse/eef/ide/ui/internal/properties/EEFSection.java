fbcec4c5e1004bf90ce3d440f5a2117c93a6df56
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-02-10 11:03:27 +0100

    Refresh section's content when the model is modified
    Change-Id: I20c99a3c8d5f666251dc4d5d689e83c911a2cd6b
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.eef.ide.ui/src/org/eclipse/eef/ide/ui/internal/properties/EEFSection.java
+++ plugins/org.eclipse.eef.ide.ui/src/org/eclipse/eef/ide/ui/internal/properties/EEFSection.java
@@ -21,11 +21,21 @@ import org.eclipse.eef.ide.ui.internal.widgets.EEFGroupLifecycleManager;
 import org.eclipse.eef.ide.ui.internal.widgets.ILifecycleManager;
 import org.eclipse.eef.properties.ui.api.EEFTabbedPropertySheetPage;
 import org.eclipse.eef.properties.ui.api.IEEFSection;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.domain.IEditingDomainProvider;
+import org.eclipse.emf.transaction.NotificationFilter;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.emf.transaction.ResourceSetListenerImpl;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * The implementation of {@link IEEFSection} using the {@link EEFSectionDescriptor}.
@@ -33,6 +43,90 @@ import org.eclipse.ui.IWorkbenchPart;
  * @author sbegaudeau
  */
 public class EEFSection implements IEEFSection {
+
+	/**
+	 * A post-commit listener which refreshes the section content when a significant change (an actual modification of a
+	 * model element) occurs in the current editing domain.
+	 */
+	private static final class SectionUpdater extends ResourceSetListenerImpl {
+		/**
+		 * Describes the changes we want to react to.
+		 */
+		private static final NotificationFilter FILTER = NotificationFilter.NOT_TOUCH.and(NotificationFilter.createEventTypeFilter(Notification.SET)
+				.or(NotificationFilter.createEventTypeFilter(Notification.UNSET)).and(NotificationFilter.createNotifierTypeFilter(EObject.class)));
+
+		/**
+		 * The section to refresh.
+		 */
+		private final IEEFSection section;
+
+		/**
+		 * Creates a new updater.
+		 *
+		 * @param section
+		 *            the section to refresh.
+		 */
+		private SectionUpdater(IEEFSection section) {
+			super(FILTER);
+			this.section = section;
+		}
+
+		@Override
+		public boolean isPostcommitOnly() {
+			return true;
+		}
+
+		@Override
+		public void resourceSetChanged(ResourceSetChangeEvent event) {
+			Display display = getCurrentDisplay();
+			if (display != null) {
+				display.asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						section.refresh();
+					}
+				});
+			}
+		}
+
+		/**
+		 * Start listening to changes from the specified editing domain.
+		 *
+		 * @param editingDomain
+		 *            the editing domain to listen to.
+		 */
+		public void attachTo(TransactionalEditingDomain editingDomain) {
+			if (editingDomain != null) {
+				editingDomain.addResourceSetListener(this);
+			}
+		}
+
+		/**
+		 * Stop listening to changes from the specified editing domain.
+		 *
+		 * @param editingDomain
+		 *            the editing domain to stop listening to.
+		 */
+		public void detachFrom(TransactionalEditingDomain editingDomain) {
+			if (editingDomain != null) {
+				editingDomain.removeResourceSetListener(this);
+			}
+		}
+
+		/**
+		 * Helper to obtain the current display, as the actual refresh must be launched in the UI thread.
+		 *
+		 * @return the current display.
+		 */
+		private Display getCurrentDisplay() {
+			if (PlatformUI.isWorkbenchRunning()) {
+				return PlatformUI.getWorkbench().getDisplay();
+			} else {
+				return Display.getDefault();
+			}
+		}
+
+	}
 
 	/**
 	 * The section descriptor.
@@ -43,6 +137,16 @@ public class EEFSection implements IEEFSection {
 	 * The lifecycle managers of this section.
 	 */
 	private List<ILifecycleManager> lifecycleManagers = new ArrayList<ILifecycleManager>();
+
+	/**
+	 * The updater which refreshes this section on external model changes.
+	 */
+	private SectionUpdater updater = new SectionUpdater(this);
+
+	/**
+	 * The editing domain of the current input.
+	 */
+	private TransactionalEditingDomain editingDomain;
 
 	/**
 	 * The constructor.
@@ -72,6 +176,7 @@ public class EEFSection implements IEEFSection {
 		for (ILifecycleManager lifecycleManager : lifecycleManagers) {
 			lifecycleManager.aboutToBeShown();
 		}
+		updater.attachTo(this.editingDomain);
 	}
 
 	@Override
@@ -84,6 +189,28 @@ public class EEFSection implements IEEFSection {
 
 			// TODO we should create a whole context with the current selection etc for the context
 			this.eefSectionDescriptor.getEEFPage().getView().setInput(input);
+			storeCurrentEditingDomain(part);
+		}
+	}
+
+	/**
+	 * Identifies the {@link TransactionalEditingDomain} assiociated with the specified part, if any, and remembers it
+	 * in {@link #editingDomain}.
+	 *
+	 * @param part
+	 *            the input part.
+	 */
+	private void storeCurrentEditingDomain(IWorkbenchPart part) {
+		IEditingDomainProvider provider = part.getAdapter(IEditingDomainProvider.class);
+		if (provider != null) {
+			EditingDomain theEditingDomain = provider.getEditingDomain();
+			if (theEditingDomain instanceof TransactionalEditingDomain) {
+				this.editingDomain = (TransactionalEditingDomain) theEditingDomain;
+			} else {
+				this.editingDomain = null;
+			}
+		} else {
+			this.editingDomain = null;
 		}
 	}
 
@@ -99,6 +226,7 @@ public class EEFSection implements IEEFSection {
 		for (ILifecycleManager lifecycleManager : lifecycleManagers) {
 			lifecycleManager.aboutToBeHidden();
 		}
+		updater.detachFrom(this.editingDomain);
 	}
 
 	@Override
