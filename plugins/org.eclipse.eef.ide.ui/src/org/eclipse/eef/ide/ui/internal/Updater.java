4cf168fa4b5c020bd99b2828a2ec548c8a5cc831
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-10-20 10:36:26 +0200

    Do not refresh the page on tree item expand/collapse
    Change-Id: I86d93a71d793c00a1e568d2c7be70d80208493b6
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.eef.ide.ui/src/org/eclipse/eef/ide/ui/internal/Updater.java
+++ plugins/org.eclipse.eef.ide.ui/src/org/eclipse/eef/ide/ui/internal/Updater.java
@@ -16,6 +16,8 @@ import org.eclipse.eef.common.ui.api.IEEFFormContainer;
 import org.eclipse.eef.core.api.controllers.IConsumer;
 import org.eclipse.eef.ide.ui.api.EEFTab;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 
 /**
  * A post-commit listener which refreshes the whole page when a significant change (an actual modification of a model
@@ -24,6 +26,14 @@ import org.eclipse.emf.common.notify.Notification;
  * @author pcdavid
  */
 public class Updater implements IConsumer<List<Notification>> {
+
+	// CHECKSTYLE:OFF
+	private static final String EXPANDED_FEATURE_NAME = "expanded"; //$NON-NLS-1$
+
+	private static final String DTREEITEM_CLASSNAME = "DTreeItem"; //$NON-NLS-1$
+
+	private static final String TREE_MM_NSURI = "http://www.eclipse.org/sirius/tree/1.0.0"; //$NON-NLS-1$
+	// CHECKSTYLE:ON
 
 	/**
 	 * The top-level page the section is part of.
@@ -65,6 +75,40 @@ public class Updater implements IConsumer<List<Notification>> {
 
 	@Override
 	public void apply(List<Notification> value) {
-		formContainer.refreshPage();
+		if (!isTreeExpansionChangeOnly(value)) {
+			formContainer.refreshPage();
+		}
+	}
+
+	/**
+	 * Checks whether the changes we are notified of only concerns Sirius tree items expansion/collapse. These are
+	 * ignored for performance reasons.
+	 *
+	 * @param changes
+	 *            the model changes.
+	 * @return <code>true</code> if the changes only concert tree items expansion state changes.
+	 */
+	private boolean isTreeExpansionChangeOnly(List<Notification> changes) {
+		for (Notification n : changes) {
+			Object src = n.getNotifier();
+			if (src instanceof EObject) {
+				EObject obj = (EObject) src;
+				String klassName = obj.eClass().getName();
+				String nsURI = obj.eClass().getEPackage().getNsURI();
+				Object feature = n.getFeature();
+				String featureName;
+				if (feature instanceof EStructuralFeature) {
+					featureName = ((EStructuralFeature) feature).getName();
+				} else {
+					featureName = ""; //$NON-NLS-1$
+				}
+				boolean isTreeExpansionChange = TREE_MM_NSURI.equals(nsURI) && DTREEITEM_CLASSNAME.equals(klassName)
+						&& EXPANDED_FEATURE_NAME.equals(featureName);
+				if (!isTreeExpansionChange) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
