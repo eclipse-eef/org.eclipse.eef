dd4108476bc03744043ac9ee157953b8fc3532e5
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2018-10-10 11:10:58 +0200

    [releng] Bump to 2.2.0
    Change-Id: Ie257120287cab8e7e48bdd4e0bbffd9177f2d610
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- releng/org.eclipse.eef.releng/publish-nightly.sh
+++ releng/org.eclipse.eef.releng/publish-nightly.sh
@@ -30,7 +30,7 @@ provided by the Hudson build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="2.1.0"
+export VERSION="2.2.0"
 export PLATFORM="oxygen"
 
 # The type of build being published
